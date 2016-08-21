1360f9d19f947496cd7a4c4cf0c989ce30c76ef9
Nathan Hardy <nhardy@live.com.au>
2016-08-21 19:37:41 +1000

    Fix some UI issues

--- src/app/components/SiteHeader/index.js
+++ src/app/components/SiteHeader/index.js
@@ -15,12 +15,14 @@ export default class SiteHeader extends Component {
     location: appPropTypes.location,
   };
 
+  static dismissEvents = ['click', 'touchstart'];
+
   componentDidMount() {
-    window.addEventListener('click', this.handleDismiss);
+    this.dismissEvents.forEach((type) => window.addEventListener(type, this.handleDismiss));
   }
 
   componentWillUnmount() {
-    window.removeEventListener('click', this.handleDismiss);
+    this.dissmissEvents.forEach((type) => window.removeEventListener(type, this.handleDismiss));
   }
 
   handleDismiss = (e) => {
@@ -40,7 +42,7 @@ export default class SiteHeader extends Component {
         </div>
         <Sticky className={styles.sticky} stickyClassName={styles.isSticky}>
           <div className={cx(styles.column, styles.navBar)}>
-            <label htmlFor="sidebarToggle" ref="label">
+            <label htmlFor="sidebarToggle" className={styles.hamburger} ref="label">
               <FontAwesome className="fa-bars" />
             </label>
             <Link to="/" className={styles.siteName}>nhardy.id.au</Link>
@@ -53,7 +55,7 @@ export default class SiteHeader extends Component {
           className={styles.checkbox}
           ref="toggle" />
         <aside className={styles.aside} ref="sidebar">
-          <label className={styles.toggle} htmlFor="sidebarToggle">
+          <label className={styles.close} htmlFor="sidebarToggle">
             <FontAwesome className="fa-close" />
           </label>
           <Nav mode="vertical" />
