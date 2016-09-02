b54719437b90be45b7724911cf219a5a19110235
Nathan Hardy <nhardy@live.com.au>
2016-09-02 13:37:46 +1000

    API tweaks

--- src/server/api/handlers/newPost.js
+++ src/server/api/handlers/newPost.js
@@ -5,16 +5,26 @@ import r from 'server/api/rethink';
 
 export default function newPostHandler(req, res, next) {
   const { text, latitude, longitude } = req.body;
+  if (!(text && latitude && longitude)) {
+    const error = new Error('Bad request');
+    error.status = 400;
+    next(error);
+    return;
+  }
+
+  const id = uuid();
   r.table('posts')
     .insert({
-      id: uuid(),
+      id,
       text,
       latitude,
       longitude,
     })
     .run()
     .then(() => {
-      res.send({ success: true });
+      res.status(201);
+      res.location(`/api/posts/${id}`);
+      res.end();
     })
     .catch(next);
 }
--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,4 +1,5 @@
 import Express from 'express';
+import bodyParser from 'body-parser';
 
 import getPostsHandler from 'server/api/handlers/getPosts';
 import newPostHandler from 'server/api/handlers/newPost';
@@ -8,7 +9,7 @@ import postHandler from 'server/api/handlers/post';
 const api = new Express();
 
 api.get('/posts', getPostsHandler);
-api.post('/posts', newPostHandler);
+api.post('/posts', bodyParser.json(), newPostHandler);
 api.get('/posts/:id', postHandler);
 
 api.use((req, res, next) => {
@@ -19,14 +20,12 @@ api.use((req, res, next) => {
 
 api.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   let { status } = err;
-  if (status !== 404) status = 500;
+  if (!status) status = 500;
 
   res.status(status);
   res.send({
-    error: {
-      status,
-      message: err.message,
-    },
+    status,
+    message: err.message,
   });
 });
 
