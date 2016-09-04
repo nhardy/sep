c922709579da1bb8f96e336b44417bc03ac4739c
Jayesh Malhotra <jayeshmal96@gmail.com>
2016-09-04 19:45:26 +1000

    Improvements based on pull request

--- src/app/components/SiteFooter/index.js
+++ src/app/components/SiteFooter/index.js
@@ -9,8 +9,8 @@ import styles from './styles.styl';
 const SiteFooter = () => (
   <footer className={styles.root}>
     <div className={styles.column}>
-      <div className={styles.heading}>
-        <span className={styles.copyright}>Copyright {config.teamName + ' ' + (new Date()).getFullYear() }</span>
+      <div className={styles.wrapper}>
+        <span className={styles.copyright}>Copyright {config.teamName } {(new Date()).getFullYear() }</span>
         <a href={config.githubUrl} target='_blank' title="Help Us Out!">
             <FontAwesome className="fa-github" />
         </a>
