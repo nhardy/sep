fc2a488d704195b14476d795b5b14c2799d099ab
Nathan Hardy <nhardy@live.com.au>
2016-10-01 13:40:05 +1000

    Misc fixes

---
+++ src/app/components/PostControls/index.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import cx from 'classnames';
+
+import Button from 'app/components/Button';
+import FontAwesome from 'app/components/FontAwesome';
+
+import styles from './styles.styl';
+
+
+@connect(null, {})
+export default class PostControls extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    hot: PropTypes.number.isRequired,
+    upVote: PropTypes.func.isRequired,
+    downVote: PropTypes.func.isRequired,
+  };
+
+  upVote = () => this.props.upVote(this.props.id);
+
+  downVote = () => this.props.downVote(this.props.id);
+
+  render() {
+    const { className, hot } = this.props;
+    return (
+      <div className={cx(styles.root, className)}>
+        <span className={styles.hot}>{hot || 0}</span>
+        <Button className={styles.button} role="button" onClick={this.upVote}>
+          <FontAwesome className="fa-arrow-circle-up" />
+        </Button>
+        <Button className={styles.button} role="button" onClick={this.downVote}>
+          <FontAwesome className="fa-arrow-circle-down" />
+        </Button>
+      </div>
+    );
+  }
+}
