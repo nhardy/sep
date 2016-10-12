eb2ba5a367f7743467a367204fa04fbbd730fc7c
Nathan Hardy <nhardy@live.com.au>
2016-10-13 09:56:55 +1100

    Fix some browser issues, clean up styles, fix favicon.ico.

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,15 @@ import config from 'app/config';
 import mainMiddleware from 'server/middleware/main';
 import errorMiddleware from 'server/middleware/error';
 
+import faviconIco from '!!buffer!app/assets/images/favicon.ico'; // eslint-disable-line
+
 
 const app = new Express();
+
+app.get('/favicon.ico', (req, res) => {
+  res.set('Content-Type', 'image/vnd.microsoft.icon');
+  res.send(faviconIco);
+});
 
 // Serve static files
 app.use('/dist', Express.static('dist'));
